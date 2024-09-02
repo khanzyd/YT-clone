9d2f43f376452e4d4fd9ea5d4e487da8638e0fec
khanzyd <khanzyd7@gmail.com>
2024-09-03 00:46:44 +0530

    add comment component

--- src/components/videoScreen/VideoComments.jsx
+++ src/components/videoScreen/VideoComments.jsx
@@ -1,35 +1,40 @@
-import React, { useEffect } from 'react'
-import request from '../../api'
-import numeral from 'numeral';
-import moment from 'moment';
-import { useDispatch } from 'react-redux';
-import { setComments } from '../../features/videos/watchVideoSlice';
+import React, { useEffect } from "react";
+import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai";
+import { BiDislike } from "react-icons/bi";
 
-const VideoComments = ({comments}) => {
-    console.log(comments);
-    
-    return (
-        <div>
-            {
-                comments.map((comment)=>{
-                    return(
-                        <div className='flex w-full bg-orange-500'>
-                            <div className="bg-red-600 w-[10%] overflow-hidden my-auto">
-                                <img src={comment.author_profileImageUrl} alt="" className='w-full rounded-full'/>
-                            </div>
-                            <div className="bg-violet-700 w-[90%] text-wrap">
-                                <div className='flex gap-3'>
-                                    <h2 className='text-sm'>{comment.author}</h2>
-                                    <p className='text-xs'>{comment.publishedAt}</p>
-                                </div>
-                                <p className='text-sm text-wrap'>{comment.text}</p>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
+const VideoComments = ({ comments }) => {
+  console.log(comments);
 
-export default VideoComments
+  return (
+    <div>
+      {comments.map((comment) => {
+        return (
+          <div className="flex w-full gap-2 my-3">
+            <div className="w-[5%] overflow-hidden">
+              <img
+                src={comment.author_profileImageUrl}
+                alt=""
+                className="w-full rounded-full"
+              />
+            </div>
+            <div className="w-[90%] text-wrap flex flex-col gap-1">
+              <div className="flex gap-3 items-center">
+                <h2 className="text-sm">{comment.author}</h2>
+                <p className="text-xs font-light">{comment.publishedAt}</p>
+              </div>
+              <div className="w-full">
+                <p className="text-sm w-full">{comment.text}</p>
+              </div>
+              <div className="flex gap-3 mt-2 mb-1">
+                  <AiOutlineLike />
+                  <BiDislike />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default VideoComments;
